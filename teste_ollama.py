2252b732a90edcb257c7294953c4cad86cbdbae4
Ronynetwork <ronyldooliveira16@gmail.com>
2025-02-11 17:32:49 -0300

    subindo teste de alteração da formatacao de requisicao

--- teste_ollama.py
+++ teste_ollama.py
@@ -6,10 +6,21 @@ erro_sq = 'Specify an exception class to catch or reraise the exception'
 
 data = {
     "model": "llama3.2:1b", 
-    "prompt": '''Erro: {}
-    codigo: 'except:'
-    me informe rapidamente descrição breve do motivo do e um único exemplo.
-    cite apenas um exemplo na explicação. Na parte de erro, explique brevemente o erro.
+    "prompt": '''
+Erro: {}
+Codigo : except:   
+
+NÃO USE ASTERISCOS NO CODIGO
+Explique brevemente o Motivo do erro e forneça um único exemplo, siga essa ordem, sempre começando com 'Exemplo:' e apenas UM exemplo, depois o motivo do erro empre começando com 'Motivo do erro:'. 
+
+Motivo do erro: 
+Explique a causa do erro de forma objetiva AQUI. Se for necessário citar o erro exato mostrado no terminal, use aspas duplas para destacá-lo. Evite informações imprecisas ou palavras inventadas.
+
+Exemplo:
+
+```python
+[código de exemplo ilustrando o erro]
+```
     '''.format(erro_sq),
     "stream": False # Retorna toda resposta em um token
 }
@@ -20,20 +31,28 @@ headers = {
 
 response = requests.post(url, json=data, headers=headers)
 
-# print("Conteúdo da resposta:", response.text)
-text = ''
+# def retornar_linhas(line_start, lines):
+#     text = ''
+#     for line in range(line_start,len(lines)):
+#         text += lines[line]
+#     print(text)
 
+# print("Conteúdo da resposta:", response.text)
 data = response.json()['response'] # Retorna os dados em string
 
+# Agora, 'lines' é uma lista com cada linha do texto como um item. sem espaços vazios
 lines = data.splitlines()
 
-ex_line = 0
-# Agora, 'lines' é uma lista com cada linha do texto como um item, percorrendo essa lista, buscando pela string especifica e salvando a linha
-for line in range(len(lines)):
-    if lines[line] == 'Exemplo:':
-        ex_line = line-1
-# Formatando para puxar do texto a linha expecifica que quero e dando split onde começa o codigo
-exemplo_aux = data[ex_line:].split('```')
-exemplo = exemplo_aux[1][6:]
+# Usando list compreenshion para retornar os valores necessários
+lines_filtered = [line.replace('**','') for line in lines if line]
 
+lines_filte_len = len(lines_filtered)
+
+# Dando join pra cada dado que vinher a partir de cada linha especificada
+exemplo = data.split('```')
+
+# Divide o conteúdo nos delimitadores ``` e pega apenas o código dentro
 print(data)
+
+line_mot = [line for line in range(lines_filte_len) if lines_filtered[line] == 'Motivo de erro:']
+print(line_mot)
