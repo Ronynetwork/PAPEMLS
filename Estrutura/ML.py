859d9d970552ac04edab140fa522c1519b330720
Ronynetwork <ronyldooliveira16@gmail.com>
2025-02-19 16:02:12 -0300

    V1.1: Ajustando e modificando tratamento do exemplo, juntamente com fixação de problemas com prompt

--- Estrutura/ML.py
+++ Estrutura/ML.py
@@ -1,26 +1,49 @@
-import requests, json, os
+import requests, json, os, html
 
 url = 'http://localhost:10012/api/generate'
-erro_sq = 'Specify an exception class to catch or reraise the exception'
+erro_sq = os.getenv('ERROR_POINT')
+
 data = {
     "model": "llama3.2:1b", 
-    "prompt": '''Erro: {}
-    codigo: except:
-    me informe rapidamente qual sentido do aviso e uma sujestao de ajuste, divida em duas partes, ERRO: e EXPLICAÇÃO:
-    cite apenas um exemplo na explicação. Na parte de erro, explique brevemente o erro mas nao cite exemplos.'''.format(erro_sq),
-    "stream": False
+    "prompt": '''
+Codigo com erro: except   
+Erro: {}
+"Explique brevemente o Motivo do erro e forneça um único exemplo, siga essa ordem, sempre começando com 'Exemplo:' e apenas UM exemplo.
+
+Exemplo:
+
+```python
+[código de exemplo ilustrando o erro]
+```
+
+ao fim do arquivo, dê uma breve explicação do porque o erro "{}" acontece na linhga de código informada forma didática e simples. Sem adicionar erros desnecessários e que não são referentes a esse erro.
+
+    '''.format(erro_sq, erro_sq),
+    "stream": False # Retorna toda resposta em um token
 }
+
 headers = {
     "Content-Type": "application/json"
 }
 
-# Enviando conteudo para IA
 response = requests.post(url, json=data, headers=headers)
-text = ''
 
-response = response.json()
-text += f"{response.get('response')}"
-print(text)
+# print("Conteúdo da resposta:", response.text)
+data = response.json()['response'] # Retorna os dados em string
+
+# Agora, 'lines' é uma lista com cada linha do texto como um item. sem espaços vazios
+lines = data.splitlines()
+
+# Usando list compreenshion para retornar os valores necessários
+lines_filtered = [line for line in lines if line]
+lines_filte_len = len(lines_filtered)
+
+# Divide o conteúdo nos delimitadores ``` e pega apenas o código dentro
+exemplo_parts = data.split("```")[1]
+
+motivo_html = [lines_filtered[line] for line in range(lines_filte_len-1,lines_filte_len)][0].replace('`', '')
+
+print(exemplo_parts)
 
 head = '''<!DOCTYPE html>
 <html lang="pt">
@@ -69,11 +92,7 @@ body = '''
 </html>
     '''
 
-message = response['message']['content']
-expliq_line = message.find('EXPLICAÇÃO:')
-#print(message)
-erro =  ''.join(message[:expliq_line-1].split('\n')).replace('*','')
-print(erro)
+
 
 option = '''
 
@@ -98,14 +117,10 @@ type_erro = '''
                     <h3> {} </h3>
                     <p>Exemplo de Correção: </p>
                     <pre>
-try:
-    x = int("abc")
-except ValueError:
-    print("Erro: Valor inválido!")
+{}
                     </pre>
                 `;
-
-'''.format(erro_sq, erro)
+'''.format(erro_sq, motivo_html, exemplo_parts)
 
 end_script = '''
             } else {
@@ -120,8 +135,8 @@ end_script = '''
 html_complete = head + option + script + type_erro + end_script + body
 
 
-with open('notification/erro.html', 'w') as arquivo:
+# Cria o diretório se ele não existir
+os.makedirs("./Estrutura/notification", exist_ok=True)
+
+with open('./Estrutura/notification/erro.html', 'w') as arquivo:
     arquivo.write(html_complete)
-    arquivo.close()
-# or access fields directly from the response object
-# print(response.message.content)
