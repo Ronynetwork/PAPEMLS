669cc4a1fcf5c578875ea287cf76442eb038fb03
Ronynetwork <ronyldooliveira16@gmail.com>
2025-02-21 13:39:17 -0300

    ADD:subindo arquivo que instala dependencias

---
+++ Estrutura/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Cria o ambiente virtual
+python3 -m venv papemls
+
+# Ativa o ambiente virtual
+source papemls/bin/activate
+
+# Instala as dependências do arquivo requirements.txt
+pip install requests pandas
