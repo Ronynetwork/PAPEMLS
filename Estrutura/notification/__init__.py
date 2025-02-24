9974e278a85bb33ad348d2889fa1afc23d648e79
Ronynetwork <ronyldooliveira16@gmail.com>
2025-02-24 16:14:53 -0300

    FIX: ajustando fetch desordenado

--- Estrutura/notification/__init__.py
+++ Estrutura/notification/__init__.py
@@ -38,4 +38,4 @@ def create_app(test_config=None):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True)
