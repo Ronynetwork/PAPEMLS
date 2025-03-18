e61a6675220d9c00987ec157cd82913dc73233f7
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-18 19:12:33 -0300

    ALT: Alterando o script com erro

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,6 +1,6 @@
 def example_function():
     divisor = 0  
-    result = 100 / divisor  # Erro crítico: divisão por zero
+    result = 100 =+ divisor
     print("Resultado:", result)
 
 example_function()
