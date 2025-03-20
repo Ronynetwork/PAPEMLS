924add3e2c86f560635433d1d1c68dc5b1fac5cf
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-20 09:54:33 -0300

    ALT: Alterando erro implantado

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,10 +1,9 @@
-def dividir(a, b):
-    return a / b  # Erro: Possível divisão por zero não tratada
-
 def main():
     x = 10
-    y = 0  # Variável com valor que causará a exceção
-    print("Divisão:", dividir(x, y))
+    soma = 0
+    
+    for i in range(x):
+        soma =+ i
 
 if __name__ == "__main__":
     main()
