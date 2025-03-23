5700c3046bf61761cbacde8b0c71836e6c1ca5dd
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-23 20:34:14 -0300

    Correction commit

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,9 +1,14 @@
-def main():
-    x = 10
-    soma = 0
-    
-    for i in range(x):
-        soma =+ i
+# Desafio: Escreva um programa que solicite a idade de 5 pessoas e mostre a soma da idades, exibindo se é maior ou menor que 60 anos.
+idade_soma = 0
 
-if __name__ == "__main__":
-    main()
+for _ in range(5):
+    idade = int(input("Informe a idade dessa pessoa: "))
+    idade_soma += idade
+
+idade_maior_60 = False
+if idade_soma > 60:
+    idade_maior_60 = True
+
+print(f"A soma das idades é {idade_soma} anos")
+print(f"É maior que 60 anos? {(idade_maior_60)}")
+
