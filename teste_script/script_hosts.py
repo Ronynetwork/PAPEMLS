ae846509e4ec2a5a04e56dd4156775cd53a55751
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-23 21:07:42 -0300

    ALT: subindo codigo com erro

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,17 +1,9 @@
-# Declaração da variável soma sem a palavra reservada
-soma = 0
-
-# Função que calcula a soma de números inteiros na faixa de -10 a 10
-def calcular_soma():
-    global soma
+def main():
+    x = 10
+    soma = 0
     
-    for i in range(-10, 11):
-        if i <= 0:
-            soma += i
-        else:
-            soma -= i
+    for i in range(x):
+        soma =+ i
 
-# Chamada da função e imprima o resultado
-calcular_soma()
-print(soma)
-
+if __name__ == "__main__":
+    main()
