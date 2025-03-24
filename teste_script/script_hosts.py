019cb75429f581e23d0b3bfe2977873da7b7e16c
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-23 21:06:21 -0300

    Correction commit

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,9 +1,17 @@
-def main():
-    x = 10
-    soma = 0
-    
-    for i in range(x):
-        soma =+ i
+# Declaração da variável soma sem a palavra reservada
+soma = 0
 
-if __name__ == "__main__":
-    main()
+# Função que calcula a soma de números inteiros na faixa de -10 a 10
+def calcular_soma():
+    global soma
+    
+    for i in range(-10, 11):
+        if i <= 0:
+            soma += i
+        else:
+            soma -= i
+
+# Chamada da função e imprima o resultado
+calcular_soma()
+print(soma)
+
