c05214137ad0dd42db6c0019be1de5f86d14e8c7
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-23 23:29:39 -0300

    FIX: Ajustando lógica do while

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,15 +1,9 @@
-# Vamos criar um programa que calcula a média de três números. 
+def main():
+    x = 10
+    soma = 0
+    
+    for i in range(x):
+        soma =+ i
 
-# Primeiro, definimos os números.
-n1 = float(input("Insira o primeiro número: "))
-n2 = float(input("Insira o segundo número: "))
-n3 = float(input("Insira o terceiro número: "))
-
-# Calculamos a soma dos números.
-soma = n1 + n2 + n3
-
-# Calculamos a média.
-media = soma / 3
-
-print(f"A média dos três números é {media}")
-
+if __name__ == "__main__":
+    main()
