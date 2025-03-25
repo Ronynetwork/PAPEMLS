491c94402297720efb0146bbe04efb72ee110422
Ronynetwork <ronyldooliveira16@gmail.com>
2025-03-25 20:10:14 -0300

    Correction commit

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,9 +1,18 @@
-def main():
-    x = 10
-    soma = 0
-    
-    for i in range(x):
-        soma =+ i
+# Declaração do resultado da soma com 0
+resultado_soma = 0
 
-if __name__ == "__main__":
-    main()
+# Funcão para calcular a soma dos números
+def soma_numeros():
+    # Declaração de parâmetros para calcular a soma
+    num1, num2 = 5, 10
+    
+    global resultado_soma  # Usando a variável global para manter o valor atualizado
+    
+    # Cálculo da soma
+    resultado_soma += (num1 + num2)
+    
+# Chamada da função para calcular a soma
+soma_numeros()
+
+print("O resultado final é:", resultado_soma)
+
