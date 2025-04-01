dc36a11794940f57e6e519b040277a2bebedb747
Ronynetwork <ronyldooliveira16@gmail.com>
2025-04-01 15:16:46 -0300

    ALT: Alterando erro para teste

--- teste_script/script_hosts.py
+++ teste_script/script_hosts.py
@@ -1,10 +1,18 @@
 def main():
     x = 10
     soma = 0
-    
-    for i in range(x):
-        soma += i
+    lista = []  # Variável não utilizada
+
+    for x in range(x):  # Shadowing da variável 'x'
+        soma =+ x  # Operador incorreto, deveria ser '+=', causando comportamento inesperado
+
+    if soma > 50:
+        return "Alto"
+    elif soma < 50:
+        return "Baixo"
+    else:
+        return soma  # Possível problema: soma pode nunca ser exatamente 50 devido ao erro acima
 
 if __name__ == "__main__":
-    main()
-
+    resultado = main()
+    print(resultado)
