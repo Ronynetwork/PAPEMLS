9694a976b9281b9772a2aad72770fe26fc7918d8
Ronynetwork <ronyldooliveira16@gmail.com>
2025-04-27 13:24:08 -0300

    DEL: Removendo diretivas de CSP

--- Estrutura/ML.py
+++ Estrutura/ML.py
@@ -62,15 +62,6 @@ head = '''<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval';
-        style-src 'self' 'unsafe-inline';
-        img-src 'self';
-        font-src 'self';
-        connect-src 'self';
-        object-src 'none';
-        frame-src 'self';">
     <link rel="stylesheet" href="style.css">
     <title>Página de Erros e Soluções</title>
 </head>
