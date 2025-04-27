4477a55e5917392b00e8df7c241b9bdfed4966ec
Ronynetwork <ronyldooliveira16@gmail.com>
2025-04-27 18:46:42 -0300

    DEBUG: Print para debug

--- Estrutura/ML.py
+++ Estrutura/ML.py
@@ -143,14 +143,16 @@ script  = script + type_erro + end_script
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Estrutura", "notification", "templates")
 static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Estrutura", "notification", "static")
 
+print(template_dir, "\n", static_dir)
+
 # Cria o diretório se ele não existir
 try:
     os.makedirs(template_dir, exist_ok=True)
     os.makedirs(static_dir, exist_ok=True)
 
     #  Criando Arquivo Js com os erros como opções
-    with open(os.path.join(static_dir, "script.js"), 'w') as arquivo:
-        arquivo.write(script)
+    with open(os.path.join(static_dir, "script.js"), 'w') as static:
+        static.write(script)
 
     # Criando HTML
     with open(os.path.join(template_dir, "erro.html"), 'w') as arquivo:
