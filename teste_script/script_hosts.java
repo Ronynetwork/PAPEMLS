b11e4f6f3bf1bf0805fae579a72983e8a01e9a94
Ronynetwork <ronyldooliveira16@gmail.com>
2025-04-29 16:59:56 -0300

    alterando codigo para simplificação

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,9 +4,6 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-
         BigDecimal bd1 = new BigDecimal(d);    // Noncompliant: Problema de precisão
-        BigDecimal bd2 = new BigDecimal(1.1);  // Noncompliant: Problema de precisão
-        BigDecimal bd3 = new BigDecimal(f);    // Noncompliant: Problema de precisão
     }
 }
