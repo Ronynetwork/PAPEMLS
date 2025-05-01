8166c4d18bac2f62f5fe3545d2d3b611928c8509
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-01 18:15:42 -0300

    FIX: Adicionando opcao de varios erros

--- Estrutura/ML.py
+++ Estrutura/ML.py
@@ -2,64 +2,90 @@ import requests, os, ast
 
 url = 'http://localhost:10012/api/generate'
 
+def type_erro(erro, motivo_html, exemplo_parts):
+    type_erro = '''
+        if (errorType === "{}") {{
+            solutionText = `
+                <h3> {} </h3>
+                <p>Exemplo de Correção: </p>
+                <pre>
+    {}
+                </pre>
+            `
+    '''.format(erro, motivo_html, exemplo_parts)
+    return type_erro
+
+def option(erro) :
+    option = '''
+                <option value="{}">Erro: {} </option>
+            </select> '''.format(erro, erro) + '''
+            <button onclick="showSolution()">Mostrar Solução</button>
+            <div id="solution" style="margin-top: 20px;"></div>
+    '''
+    return option
+
 try:        
     erro_sq = os.getenv('ERROR_POINT')
     erro_dict = ast.literal_eval(erro_sq)
     print('Erro dict: ', erro_dict)
+    options = ''
+    types = ''
     for dic in erro_dict:
-        print(dic)
         for erro, code in dic.items():
             print(f"Erro: {erro}, Código: {code}")
             erro = erro.replace('"', '')
             code = code
+            data = {
+                "model": "llama3.2:1b", 
+                "prompt": '''
+            Codigo com erro: {}   
+            Erro: {}
+            "Explique brevemente o Motivo do erro e forneça um único exemplo, siga essa ordem, sempre começando com 'Exemplo:' e apenas UM exemplo.
+
+            Exemplo:
+
+            ```
+            [código de exemplo resolvendo de forma simples o erro]
+            ```
+
+            ao fim do arquivo, dê uma breve explicação do porque o erro "{}" acontece na linha de código informada forma didática e simples. Sem adicionar erros desnecessários e que não são referentes a esse erro.
+
+                '''.format(code, erro, erro),
+                "stream": False  # Retorna toda resposta em um token
+            }
+
+            headers = {
+                "Content-Type": "application/json"
+            }
+
+            response = requests.post(url, json=data, headers=headers)
+            print(response)
+
+            # print("Conteúdo da resposta:", response.text)
+            if response: 
+                data = response.json()['response'] # Retorna os dados em string
+            else:
+                print("reponse inexistente")
+            # Agora, 'lines' é uma lista com cada linha do texto como um item. sem espaços vazios
+            print(data)
+            lines = data.splitlines()
+
+            # Usando list compreenshion para retornar os valores necessários
+            lines_filtered = [line for line in lines if line]
+            lines_filte_len = len(lines_filtered)
+
+            # Divide o conteúdo nos delimitadores ``` e pega apenas o código dentro
+            exemplo_parts = data.split("```")[1]
+            motivo_html = [lines_filtered[line] for line in range(lines_filte_len-1,lines_filte_len)][0].replace('`', '')
+            print(exemplo_parts)
+
+            options += option(erro)
+            types += type_erro(erro, motivo_html, exemplo_parts)
     else:   
         print("Variável ERROR_POINT não encontrada ou vazia")
 except Exception as e:
     print("Projeto nao possue issues abertas!")
 
-data = {
-    "model": "llama3.2:1b", 
-    "prompt": '''
-Codigo com erro: {}   
-Erro: {}
-"Explique brevemente o Motivo do erro e forneça um único exemplo, siga essa ordem, sempre começando com 'Exemplo:' e apenas UM exemplo.
-
-Exemplo:
-
-```
-[código de exemplo resolvendo de forma simples o erro]
-```
-
-ao fim do arquivo, dê uma breve explicação do porque o erro "{}" acontece na linha de código informada forma didática e simples. Sem adicionar erros desnecessários e que não são referentes a esse erro.
-
-    '''.format(code, erro, erro),
-    "stream": False  # Retorna toda resposta em um token
-}
-
-headers = {
-    "Content-Type": "application/json"
-}
-
-response = requests.post(url, json=data, headers=headers)
-print(response)
-
-# print("Conteúdo da resposta:", response.text)
-if response: 
-    data = response.json()['response'] # Retorna os dados em string
-else:
-    print("reponse inexistente")
-# Agora, 'lines' é uma lista com cada linha do texto como um item. sem espaços vazios
-print(data)
-lines = data.splitlines()
-
-# Usando list compreenshion para retornar os valores necessários
-lines_filtered = [line for line in lines if line]
-lines_filte_len = len(lines_filtered)
-
-# Divide o conteúdo nos delimitadores ``` e pega apenas o código dentro
-exemplo_parts = data.split("```")[1]
-motivo_html = [lines_filtered[line] for line in range(lines_filte_len-1,lines_filte_len)][0].replace('`', '')
-print(exemplo_parts)
 
 head = '''<!DOCTYPE html>
 <html lang="pt">
@@ -77,18 +103,12 @@ head = '''<!DOCTYPE html>
 '''
 
 body = '''
+    </div>
+    <script src="{{url_for('static', filename='script.js')}}"></script>
 </body>
 </html>
     '''
 
-option = '''
-            <option value="{}">Erro: {} </option>
-        </select> '''.format(erro, erro) + '''
-        <button onclick="showSolution()">Mostrar Solução</button>
-        <div id="solution" style="margin-top: 20px;"></div>
-    </div>
-    <script src="{{url_for('static', filename='script.js')}}"></script>
-'''
     
 script ='''
 function showSolution() {
@@ -98,21 +118,10 @@ function showSolution() {
     let solutionText = "";'''
     
 
-type_erro = '''
-    if (errorType === "{}") {{
-        solutionText = `
-            <h3> {} </h3>
-            <p>Exemplo de Correção: </p>
-            <pre>
-{}
-            </pre>
+end_script = '''
         <h2>Escolha uma ação:</h2>
         <button onclick="enviarAcao('corrigir')">Corrigir</button>
         <button onclick="enviarAcao('ignorar')">Ignorar</button>
-        `
-'''.format(erro, motivo_html, exemplo_parts)
-
-end_script = '''
     } else {
         solutionText = "<p>Selecione um erro para ver a solução.</p>";
     }
@@ -133,8 +142,8 @@ function enviarAcao(acao) {
 }
 '''
 
-html_complete = head + option + body
-script  = script + type_erro + end_script
+html_complete = head + options + body
+script  = script + types + end_script
 
 # Cria o diretório se ele não existir
 try:
