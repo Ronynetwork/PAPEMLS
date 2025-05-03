015965772ae52d09d1d413b9022c9a99b92af96b
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-03 20:40:44 -0300

    DEBUG: Print para debug

--- Estrutura/ML_autocorrigir.py
+++ Estrutura/ML_autocorrigir.py
@@ -16,6 +16,39 @@ try:
                 if erro_limpo in err:
                     code = err[erro_limpo]
                     print(f"Erro para ser corrigido: {erro}, Código: {code}")
+
+                    data = {
+                        "model": "llama3.2:1b", 
+                        "prompt": '''
+                    Codigo com erro: 
+
+                    {}
+                            
+                    Erro: {}
+
+                    ajuste ao código para apenas uma versao corrigida e funcional, nao preciso de explicação, apenas do codigo corrigido e funcional, seja o  mais assertivo possível.
+
+                        '''.format(code, erro),
+                        "stream": False # Retorna toda resposta em um token
+                    }
+
+                    headers = {
+                        "Content-Type": "application/json"
+                    }
+
+                    response = requests.post(url, json=data, headers=headers)
+                    if response: 
+                        data = response.json()['response'] # Retorna os dados em string
+                        data_split = data.split('```')[1].split("\n")
+                        data_join = [f'{data_split[x]}\n' for x in range(1, len(data_split))]
+
+                        file_path = os.path.abspath("teste_script/script_hosts.java")
+                        print("Tentando acessar:", file_path)
+                        with open(file_path, "w") as f:
+                            for x in data_join:
+                                f.write(x)
+                    else:
+                        print("response inexistente")
     else:   
         print("Variável ERROR_POINT não encontrada ou vazia")
 except Exception as e:
@@ -23,40 +56,6 @@ except Exception as e:
     exit(0)
 
 
-data = {
-    "model": "llama3.2:1b", 
-    "prompt": '''
-Codigo com erro: 
-
-{}
-           
-Erro: {}
-
-ajuste ao código para apenas uma versao corrigida e funcional, nao preciso de explicação, apenas do codigo corrigido e funcional, seja o  mais assertivo possível.
-
-    '''.format(code, erro),
-    "stream": False # Retorna toda resposta em um token
-}
-
-headers = {
-    "Content-Type": "application/json"
-}
-
-response = requests.post(url, json=data, headers=headers)
-
 # Dando join pra cada dado que vinher a partir de cada linha especificada
 
 # Divide o conteúdo nos delimitadores ``` e pega apenas o código dentro
-
-if response: 
-    data = response.json()['response'] # Retorna os dados em string
-    data_split = data.split('```')[1].split("\n")
-    data_join = [f'{data_split[x]}\n' for x in range(1, len(data_split))]
-
-    file_path = os.path.abspath("teste_script/script_hosts.java")
-    print("Tentando acessar:", file_path)
-    with open(file_path, "w") as f:
-        for x in data_join:
-            f.write(x)
-else:
-    print("response inexistente")
