ed3e8f8d7a65bbd197146018ccb10ece945380dd
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-04 11:35:37 -0300

    Correction commit

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,6 +4,11 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-        BigDecimal bd1 = new BigDecimal(d);    // Noncompliant: Problema de precisão
+
+        BigDecimal bd = new BigDecimal(d);
+        BigDecimal bd2 = new BigDecimal(f);
+
+        System.out.println(bd.equals(bd2)); // Verifica se os valores são iguais
     }
 }
+
