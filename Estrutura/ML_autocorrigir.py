79d00d8d41b3af5857b6ef6c9420953206bb1cee
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-03 21:25:25 -0300

    DEBUG: Print para debug

--- Estrutura/ML_autocorrigir.py
+++ Estrutura/ML_autocorrigir.py
@@ -5,15 +5,13 @@ url = 'http://localhost:10012/api/generate'
 try:
     erro_dict = ast.literal_eval(os.getenv("ERROR_POINT"))
     erros_escolhidos = ast.literal_eval(os.getenv("ERROS"))
-    if erro_dict:
-        print(erro_dict)
-        print(erros_escolhidos)
-        for erro in erros_escolhidos:
-            for err in erro_dict:
-                print(err)
-                print(erro)
-                if erro in err:
-                    code = err[erro]
+    for erro in erros_escolhidos:
+        erro_limpo = erro.replace('"', '').strip()
+        for err in erro_dict:
+            for chave in err.keys():
+                chave_limpa = chave.replace('"', '').strip()
+                if erro_limpo == chave_limpa:
+                    code = err[chave]
                     print(f"Erro para ser corrigido: {erro}, Código: {code}")
 
                     data = {
