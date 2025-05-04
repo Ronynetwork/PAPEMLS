d2742c37eeeb4c11c0948b6217387f3e8def7f91
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-04 12:09:46 -0300

    testando codigo com erro

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,11 +4,6 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-
-        BigDecimal bd = new BigDecimal(d);
-        BigDecimal bd2 = new BigDecimal(f);
-
-        System.out.println(bd.equals(bd2)); // Verifica se os valores são iguais
+        BigDecimal bd1 = new BigDecimal(d);    // Noncompliant: Problema de precisão
     }
 }
-
