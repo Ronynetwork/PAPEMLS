16c736dd503904dbd81fff1fc8b5af0ead5dbf4f
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-04 12:52:28 -0300

    Correction commit

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,6 +4,7 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-        BigDecimal bd1 = new BigDecimal(d);    // Noncompliant: Problema de precisão
+        BigDecimal bd1 = new BigDecimal(d).scale(10);
     }
 }
+
