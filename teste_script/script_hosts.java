ebf4add044698ba370a8cbbcacfa548b81832dba
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-04 14:01:31 -0300

    DEBUG: Print para debug

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,11 +4,10 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-        
-        // Utilizar BigDecimal.valueOf() para obter a representação decimal específica de f
-        BigDecimal bd1 = new BigDecimal(f);
-        
-        // Para evitar problemas com precisão de float, podemos usar BigDecimal.valueOf(f) aqui.
+        BigDecimal bd1 = new BigDecimal(d);
+        // BigDecimal.valueOf() não é necessário em Java 7+ e pode causar problemas
+        // com precisão de float (float é menor que double, então não é possível usar BigDecimal)
+        // Para evitar problemas, podemos usar BigDecimal.valueOf(f) aqui
     }
 }
 
