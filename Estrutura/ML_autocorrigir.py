ff33b9efbbb9a2d229b6e5f6503a149073a6dab1
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-03 21:37:49 -0300

    DEBUG: Print para debug

--- Estrutura/ML_autocorrigir.py
+++ Estrutura/ML_autocorrigir.py
@@ -3,6 +3,9 @@ import requests, os, ast
 url = 'http://localhost:10012/api/generate'
 
 try:
+    file_path = os.path.abspath("teste_script/script_hosts.java")
+    with open(file_path, "r") as f:
+        code = f.read()
     erro_dict = ast.literal_eval(os.getenv("ERROR_POINT"))
     erros_escolhidos = ast.literal_eval(os.getenv("ERROS"))
     for erro in erros_escolhidos:
@@ -11,7 +14,6 @@ try:
             for chave in err.keys():
                 chave_limpa = chave.replace('"', '').strip()
                 if erro_limpo == chave_limpa:
-                    code = err[chave]
                     print(f"Erro para ser corrigido: {erro}, Código: {code}")
 
                     data = {
@@ -32,14 +34,15 @@ try:
                     headers = {
                         "Content-Type": "application/json"
                     }
-
+                    print(f"chegou aq")
                     response = requests.post(url, json=data, headers=headers)
+
+                    print(response.status_code, response)
                     if response: 
                         data = response.json()['response'] # Retorna os dados em string
                         data_split = data.split('```')[1].split("\n")
                         data_join = [f'{data_split[x]}\n' for x in range(1, len(data_split))]
 
-                        file_path = os.path.abspath("teste_script/script_hosts.java")
                         print("Tentando acessar:", file_path)
                         with open(file_path, "w") as f:
                             for x in data_join:
