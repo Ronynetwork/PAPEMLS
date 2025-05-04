016b892a8428a2f85d919d2abefea83926ac112c
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-04 12:41:33 -0300

    Correction commit

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,6 +4,16 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-        BigDecimal bd1 = new BigDecimal(d);    // Noncompliant: Problema de precisão
+
+        // Corrigir a declaração de bd1 para evitar o uso de um local não usado.
+        BigDecimal bd1 = new BigDecimal(d);
+        
+        // Verificar que BD1 não contenha valor nulo
+        if (bd1 != null) {
+            System.out.println("BD1 " + bd1);
+        } else {
+            System.out.println("BD1 é nuló");
+        }
     }
 }
+
