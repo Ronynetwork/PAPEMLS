15453b5a7546fcd788e820268af495fd3ec056e6
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-04 12:56:54 -0300

    FIX: Adicionando sleep para iniciação do flask

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -4,7 +4,6 @@ public class TesteBigDecimalErro {
     public static void main(String[] args) {
         double d = 1.1;
         float f = 2.2f;
-        BigDecimal bd1 = new BigDecimal(d).scale(10);
+        BigDecimal bd1 = new BigDecimal(d);    // Noncompliant: Problema de precisão
     }
 }
-
