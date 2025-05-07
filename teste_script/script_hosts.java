59ecc80e8537838bbd60a7cb1be1ca5c79a2b74e
Ronynetwork <ronyldooliveira16@gmail.com>
2025-05-07 10:10:55 -0300

    ALT: Adicionando code smell para análise

--- teste_script/script_hosts.java
+++ teste_script/script_hosts.java
@@ -1,13 +1,8 @@
-import java.math.BigDecimal;
+import java.util.*;
 
-public class TesteBigDecimalErro {
+public class Example {
     public static void main(String[] args) {
-        double d = 1.1;
-        float f = 2.2f;
-        BigDecimal bd1 = new BigDecimal(d);
-        // BigDecimal.valueOf() não é necessário em Java 7+ e pode causar problemas
-        // com precisão de float (float é menor que double, então não é possível usar BigDecimal)
-        // Para evitar problemas, podemos usar BigDecimal.valueOf(f) aqui
+        List list = Collections.EMPTY_LIST;
+        list.add("John");  // Lança UnsupportedOperationException
     }
 }
-
