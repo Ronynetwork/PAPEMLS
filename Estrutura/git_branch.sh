0847ccb78610d468511123279821bb1598302ba4
Ronynetwork <rony@DESKTOP-LKVLDBK.localdomain>
2025-10-05 23:57:25 -0300

    FIX: Ajustando a branch

--- Estrutura/git_branch.sh
+++ Estrutura/git_branch.sh
@@ -3,15 +3,15 @@
 git remote set-url origin git@github.com:Ronynetwork/PAPEMLS.git
 
 # Conferindo se a branch main existe, se não, cria
-git checkout main
-git pull origin main
+git checkout dev
+git pull origin dev
 
 # Adicionar apenas o arquivo desejado
 git add ./teste_script/
 git restore .
 
 # Fazer o commit com uma mensagem
-git commit -m "Correction commit"
+git commit -m "FIX: Correction commit"
 
 # Enviar as mudanças para o repositório remoto
-git push origin main
+git push origin dev
