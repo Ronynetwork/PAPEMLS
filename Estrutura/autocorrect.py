7753355c45bc58ae32cb7ce7a748e2a8e01e5b7e
Ronynetwork <rony@DESKTOP-LKVLDBK.localdomain>
2025-10-05 22:07:48 -0300

    DEBUG: Teste de recebimento dos erros

--- Estrutura/autocorrect.py
+++ Estrutura/autocorrect.py
@@ -4,6 +4,8 @@ import os
 # Receber os dados do Jenkins com os erros e arquivos a serem corrigidos
 errosDict = os.getenv("ERROS")
 atualPath = os.path.abspath(os.getcwd()) # Pega o caminho atual
+print("Caminho Atual: ", atualPath)
+print("Erros Dict: ", errosDict)
 
 
 # Alteração de arquivos
