11de876ebc7022f0ce91cbf6222a89c1c5321669
Ronynetwork <rony@DESKTOP-LKVLDBK.localdomain>
2025-10-19 17:34:44 -0300

    ADD: Adicionando tentativa de tratamento de primeiro erro

--- Estrutura/autocorrect.py
+++ Estrutura/autocorrect.py
@@ -21,6 +21,13 @@ try:
             line = erro['line']
             corrections = erro['correction']
             print(f"Mensagem: {message}, Linha: {line}, Correções: {corrections}")
+
+            with open(path, 'w') as file:
+                file_lines = file.splitlines()
+                for l in file_lines:
+                    if l == line:
+                        file.writelines(corrections)
+                print(f"Arquivo {path} corrigido com sucesso.")
 except Exception as e:
     print("Erro ao processar a lista de erros: ", e)
 # Alteração de arquivos
