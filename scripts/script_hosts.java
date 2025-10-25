10f3e42b2e9f3671e2b57ab3c5baa2392b304fc8
Ronyldo Oliveira <122325315+Ronynetwork@users.noreply.github.com>
2025-10-25 14:06:13 -0300

    Reinserindo erro

--- scripts/script_hosts.java
+++ scripts/script_hosts.java
@@ -6,8 +6,8 @@ import java.io.ByteArrayInputStream;
 public class XXEVuln {
     public static void main(String[] args) throws Exception {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-dbf.setFeature("http://apache.org/xml/features/disallow-external-entities", true);        String xml = "<!DOCTYPE foo [ <!ELEMENT foo ANY > <!ENTITY xxe SYSTEM \"file:///etc/passwd\" >]><foo>&xxe;</foo>";
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        String xml = "<!DOCTYPE foo [ <!ELEMENT foo ANY > <!ENTITY xxe SYSTEM \"file:///etc/passwd\" >]><foo>&xxe;</foo>";
         Document doc = db.parse(new ByteArrayInputStream(xml.getBytes()));  // Ativa a regra S2755
-
-deve ser:}
+    }
+}
