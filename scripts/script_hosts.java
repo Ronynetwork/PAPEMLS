467784b452ffc52a3999867e3856051592f91b7c
Ronynetwork <ronyldooliveira16@gmail.com>
2025-10-25 15:38:33 -0300

    FIX: Ajustando retorno de erro via mL

--- scripts/script_hosts.java
+++ scripts/script_hosts.java
@@ -8,5 +8,6 @@ public class XXEVuln {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         String xml = "<!DOCTYPE foo [ <!ELEMENT foo ANY > <!ENTITY xxe SYSTEM \"file:///etc/passwd\" >]><foo>&xxe;</foo>";
-Document doc = db.parse(new ByteArrayInputStream(xml.getBytes()));  // Ativa a regra S2755    }
+        Document doc = db.parse(new ByteArrayInputStream(xml.getBytes()));  // Ativa a regra S2755    
+    }
 }
