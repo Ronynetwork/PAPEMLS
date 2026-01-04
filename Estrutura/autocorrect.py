35313da71395b295bbc63d97738126548153da53
Ronynetwork <ronyldooliveira16@gmail.com>
2026-01-04 17:19:45 -0300

    FIX: Ajustando tratamento de path do arquivo

--- Estrutura/autocorrect.py
+++ Estrutura/autocorrect.py
@@ -6,14 +6,14 @@ pathList_raw = os.getenv("ERROS")  # isso é uma string
 pathList = json.loads(pathList_raw)  # agora é uma lista de dicionários
 atualPath = os.path.abspath(os.getcwd()) # Pega o caminho atual
 print("Caminho Atual: ", atualPath)
-print("Caminho do arquivo: ")
 print("Path list: ", pathList)
 
 # Exibir os dados recebidos para verificação
 try:
     for obj in pathList:    
-        path = obj['path']
-        print("Path atual: ", path)
+        pathBrute = obj['path']
+        pathFinal = pathBrute.split(":")[1]
+        print("Path atual: ", pathFinal)
 
         for erro in obj['errors']: # Percorre a lista de erros e define as correções
             # print("Erro: ", erro)
@@ -22,7 +22,7 @@ try:
             correction = erro['correction']
             # print(f"Mensagem: {message}, Linha: {line}, Correções: {corrections}")
 
-            with open(path, 'r+') as file: # Abre o arquivo para escrita e itera sobre as linhas para aplicar as correções
+            with open(pathFinal, 'r+') as file: # Abre o arquivo para escrita e itera sobre as linhas para aplicar as correções
                 file_lines = file.readlines()
                 print("Linhas do arquivo antes da correção: ", file_lines)
                 for l, content in enumerate(file_lines, start=1): # Percorre cada linha do arquivo
@@ -38,7 +38,7 @@ try:
                 file.seek(0) # Move o cursor para o início do arquivo
                 file.writelines(file_lines) # Escreve as linhas corrigidas de volta ao arquivo
                 file.truncate() # Remove qualquer conteúdo restante após a escrita
-                print(f"Arquivo {path} corrigido com sucesso.")
+                print(f"Arquivo {pathFinal} corrigido com sucesso.")
 except Exception as e:
     print("Erro ao processar a lista de erros   : ", e)
 # Alteração de arquivos
