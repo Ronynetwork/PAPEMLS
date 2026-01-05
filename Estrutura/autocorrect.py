fa4d6a5cda949a112e96ef369fadbf1eb171ee07
Ronynetwork <ronyldooliveira16@gmail.com>
2026-01-04 22:49:31 -0300

    Ajustando logging

--- Estrutura/autocorrect.py
+++ Estrutura/autocorrect.py
@@ -25,20 +25,10 @@ try:
 
             with open(pathFinal, 'r+') as file: # Abre o arquivo para escrita e itera sobre as linhas para aplicar as correções
                 file_lines = file.readlines()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                 logger.info("Linhas do arquivo antes da correção: ", file_lines)
                 for l, content in enumerate(file_lines, start=1): # Percorre cada linha do arquivo onde L vira a linha e content o conteudo
-=======
-                print("Linhas do arquivo antes da correção: ", file_lines)
-                for l, content in enumerate(file_lines, start=1): # Percorre cada linha do arquivo
-                    print("Linha atual do arquivo: ", content, " Linha do erro: ", l, " Linha para correção: ", line)
->>>>>>> parent of 2f01a6c (FIX: Ajustando tratamento de path do arquivo)
                     if l == line:
-
                         # Corrigindo identação antes de alterar
-<<<<<<< HEAD
                         logger.info(f"Aplicando correção na linha {line}: {correction}")
 
             # captura os espaços/tabs iniciais com método GROUP e utiliza -1 nas linhas do arquivo indicando a linha exata de alteração
@@ -47,40 +37,9 @@ try:
                         file_lines[l-1] =  correction_complete # Substitui a linha com a correção e adiciona quebra de linha
 
                 logger.info("Linhas do arquivo após a correção: ", file_lines)
-=======
-                        print(f"Aplicando correção na linha {line}: {correction}")
-                        indentacao = re.match(r'^\s*', file_lines[l-1]).group()  # captura os espaços/tabs iniciais
-                        correction_complete = indentacao + correction + '\n'
-                        file_lines[l-1] =  correction_complete # Substitui a linha com a correção e adiciona quebra de linha
-=======
-                print("Linhas do arquivo antes da correção: ", file_lines)
-                for l, content in enumerate(file_lines, start=1): # Percorre cada linha do arquivo
-                    print("Linha atual do arquivo: ", content, " Linha do erro: ", l, " Linha para correção: ", line)
-                    if l == line:
-
-                        # Corrigindo identação antes de alterar
-                        print(f"Aplicando correção na linha {line}: {correction}")
-                        indentacao = re.match(r'^\s*', file_lines[l-1]).group()  # captura os espaços/tabs iniciais
-                        correction_complete = indentacao + correction + '\n'
-                        file_lines[l-1] =  correction_complete # Substitui a linha com a correção e adiciona quebra de linha
->>>>>>> parent of 2f01a6c (FIX: Ajustando tratamento de path do arquivo)
-=======
-                print("Linhas do arquivo antes da correção: ", file_lines)
-                for l, content in enumerate(file_lines, start=1): # Percorre cada linha do arquivo
-                    print("Linha atual do arquivo: ", content, " Linha do erro: ", l, " Linha para correção: ", line)
-                    if l == line:
-
-                        # Corrigindo identação antes de alterar
-                        print(f"Aplicando correção na linha {line}: {correction}")
-                        indentacao = re.match(r'^\s*', file_lines[l-1]).group()  # captura os espaços/tabs iniciais
-                        correction_complete = indentacao + correction + '\n'
-                        file_lines[l-1] =  correction_complete # Substitui a linha com a correção e adiciona quebra de linha
->>>>>>> parent of 2f01a6c (FIX: Ajustando tratamento de path do arquivo)
-                print("Linhas do arquivo após a correção: ", file_lines)
->>>>>>> parent of 2f01a6c (FIX: Ajustando tratamento de path do arquivo)
                 file.seek(0) # Move o cursor para o início do arquivo
                 file.writelines(file_lines) # Escreve as linhas corrigidas de volta ao arquivo
                 file.truncate() # Remove qualquer conteúdo restante após a escrita
-                logger.info(f"Arquivo {pathFinal} corrigido com sucesso.")
+                print(f"Arquivo {pathFinal} corrigido com sucesso.")
 except Exception as e:
-    logger.error("Erro ao processar a lista de erros   : ", e)
+    print("Erro ao processar a lista de erros   : ", e)
