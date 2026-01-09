2f1615c433a8083027ca162248a1ca375c99320d
Jenkins <jenkins@correction>
2026-01-09 15:46:03 -0300

    FIX: Correction commit

--- scripts/script_hosts.java
+++ scripts/script_hosts.java
@@ -8,6 +8,14 @@ public class XXEVulnerable {
 
     public static void main(String[] args) throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+factory.setValidating(false);
+factory.setXIncludeAware(false);
+factory.setExpandEntityReferences(false); 
+factory.setNamespaceAware(true);
+factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 
         // ❌ Configuração insegura (XXE habilitado por padrão)
         DocumentBuilder builder = factory.newDocumentBuilder();
